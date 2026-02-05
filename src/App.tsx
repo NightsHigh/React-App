2296c756472fe5006d774e73fddd734c47b08c5e
NightsHigh <soren913@gmail.com>
2026-02-05 13:59:33 +0100

    Added calculator in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,6 @@
-import ThemeContextProvider, { useTheme } from "./components/Context";
-
-function AppContent() {
-  const { theme, setTheme } = useTheme();
-
+import { Calculator } from "./components/Calculator";
+export default function App() {
   return (
-    <div
-      className={`min-h-screen flex flex-col ${
-        theme === "light" ? "bg-white text-black" : "bg-black text-white"
-      }`}
-    >
-      <h1 className={theme === "light" ? "text-black" : "text-white"}>ThemeContext: {theme}</h1>
-      <button
-        className="bg-blue-500 text-white p-2 rounded-md"
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      >
-        Toggle Theme
-      </button>
-    </div>
+    <Calculator />
   );
 }
-
-function App() {
-  return (
-    <ThemeContextProvider>
-      <AppContent />
-    </ThemeContextProvider>
-  );
-}
-
-export default App;
